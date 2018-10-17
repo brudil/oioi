c86545abaf726b54b0cb2288ce2f2472a51a22a8
James Canning <james@brudil.com>
2018-10-17 12:30:23 +0100

    Ensure src/generated exists before generation

--- scripts/generate-grammar.sh
+++ scripts/generate-grammar.sh
@@ -1,3 +1,4 @@
+mkdir -p src/generated
 nearleyc src/grammar.ne -o src/generated/grammar.ts
 sed '/@preprocessor typescript/d' ./src/grammar.ne > ./src/generated/grammar-nopost.ne
 nearleyc src/generated/grammar-nopost.ne -o src/generated/grammar.js
